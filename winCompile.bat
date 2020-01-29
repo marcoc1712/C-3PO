874ff5c522526a56af4f191e207439a8c15dea76
marcoc1712 <marco1712@gmail.com>
2020-01-29 18:51:32 +0100

    More space for extra command

---
+++ winCompile.bat
@@ -0,0 +1,3 @@
+cd F:\slimserver\Plugins\C3PO
+del F:\slimserver\Plugins\C3PO\Bin\MSWin32-x86-multi-thread\C-3PO.exe
+pp -a lib C-3PO.pl -o F:\slimserver\Plugins\C3PO\Bin\MSWin32-x86-multi-thread\C-3PO.exe
